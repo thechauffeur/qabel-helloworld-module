9269f27ef086271350c7a325268c62d23f8bd5f2
L-Henke <henke@praemandatum.de>
2015-06-09 13:32:22 +0200

    Adapt to refactored DropActor
    qabel-helloworld-module example hasn't been working and the test has
    been commented out. This commit just allows to compile this module with
    the refactored of the DropActor.
    
    After the ModuleManager has been refactored, this example will be
    working again.

--- src/main/java/de/qabel/helloworld/QblHelloWorldModule.java
+++ src/main/java/de/qabel/helloworld/QblHelloWorldModule.java
@@ -3,11 +3,18 @@ package de.qabel.helloworld;
 import de.qabel.ackack.event.EventEmitter;
 import de.qabel.core.config.Contact;
 import de.qabel.core.config.Entity;
+import de.qabel.core.config.Identity;
+import de.qabel.core.crypto.QblECKeyPair;
 import de.qabel.core.drop.DropActor;
 import de.qabel.core.drop.DropMessage;
+import de.qabel.core.drop.DropURL;
 import de.qabel.core.drop.ModelObject;
+import de.qabel.core.exceptions.QblDropInvalidURL;
 import de.qabel.core.module.Module;
 
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 
 public class QblHelloWorldModule extends Module {
@@ -41,10 +48,32 @@ public class QblHelloWorldModule extends Module {
 
 		Entity sender = null; // TODO
         DropMessage<HelloWorldObject> dm = new DropMessage<>(sender, data);
-		HashSet<Contact> contacts = new HashSet<>(this.getModuleManager().getDropActor().getContacts().getContacts());
 
-		DropActor.send(EventEmitter.getDefault(), dm, contacts);
-    }
+		/*
+		 TODO: This is taken from MultiPartCryptoTest just to compile this example again.
+		 TODO: A working example will be created after a refactorization of the ModuleManager
+		 */
+		try {
+			QblECKeyPair alicesKey = new QblECKeyPair();
+			Collection<DropURL> alicesDrops = new ArrayList<DropURL>();
+			alicesDrops.add(new DropURL("http://localhost:6000/12345678901234567890123456789012345678alice"));
+			Identity alice = new Identity("Alice", alicesDrops, alicesKey);
+
+			QblECKeyPair bobsKey = new QblECKeyPair();
+			Identity bob = new Identity("Bob", new ArrayList<DropURL>(), bobsKey);
+			bob.addDrop(new DropURL("http://localhost:6000/1234567890123456789012345678901234567890bob"));
+
+			Contact alicesContact = new Contact(alice, null, bobsKey.getPub());
+			alicesContact.addDrop(new DropURL("http://localhost:6000/1234567890123456789012345678901234567890bob"));
+
+			Contact bobsContact = new Contact(bob, null, alicesKey.getPub());
+			alicesContact.addDrop(new DropURL("http://localhost:6000/12345678901234567890123456789012345678alice"));
+
+			DropActor.send(EventEmitter.getDefault(), dm, bobsContact);
+		} catch (MalformedURLException | QblDropInvalidURL e) {
+			e.printStackTrace();
+		}
+	}
 
     protected void onDropMessage(DropMessage<?> dm) {
         System.out.println(dm.getData().as(HelloWorldObject.class).getStr());
