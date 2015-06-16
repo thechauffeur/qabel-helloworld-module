c202f7e6ce3c844c2b2d9ca5731468a43940c342
L-Henke <henke@praemandatum.de>
2015-06-16 13:21:57 +0200

    Add a Contact for hello world test

--- src/test/java/QblHelloWorldModuleTest.java
+++ src/test/java/QblHelloWorldModuleTest.java
@@ -1,6 +1,8 @@
 import de.qabel.ackack.event.EventEmitter;
-import de.qabel.core.config.Persistence;
+import de.qabel.core.config.*;
+import de.qabel.core.crypto.QblECKeyPair;
 import de.qabel.core.drop.DropActor;
+import de.qabel.core.drop.DropURL;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,11 +10,33 @@ import de.qabel.core.module.ModuleManager;
 import de.qabel.helloworld.QblHelloWorldModule;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class QblHelloWorldModuleTest {
+	private static final String BOB_DROP_URL = "http://localhost:6000/123456789012345678901234567890123456789012b";
+	private ModuleManager moduleManager;
+
 	@Before
 	public void setUp() throws Exception {
 		Persistence.setPassword("qabel".toCharArray());
+		moduleManager = new ModuleManager();
+
+		Collection<DropURL> bobDropURLs = new ArrayList<>();
+		bobDropURLs.add(new DropURL(BOB_DROP_URL));
+
+		Identity alice = new Identity("Alice", null, new QblECKeyPair());
+		Identity bob = new Identity("Bob", bobDropURLs, new QblECKeyPair());
+
+		Identities identities = new Identities();
+		identities.put(alice);
+		moduleManager.getConfigActor().writeIdentities(identities.getIdentities().toArray(new Identity[0]));
+
+		Contact bobAsContactForAlice = new Contact(alice, bobDropURLs, bob.getEcPublicKey());
+
+		Contacts contacts = new Contacts();
+		contacts.put(bobAsContactForAlice);
+		moduleManager.getContactsActor().writeContacts(contacts.getContacts().toArray(new Contact[0]));
 
 		DropActor dropActor = new DropActor(EventEmitter.getDefault());
 		Thread dropActorThread = new Thread(dropActor);
@@ -21,10 +45,8 @@ public class QblHelloWorldModuleTest {
 	}
 
 	@Test
-	public void test() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InterruptedException {
-		ModuleManager mm = new ModuleManager();
-		mm.startModule(QblHelloWorldModule.class);
-		mm.shutdown();
+	public void helloWorldModuleTest() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InterruptedException {
+		moduleManager.startModule(QblHelloWorldModule.class);
+		moduleManager.shutdown();
 	}
-
 }
